5657a4e651873c60915a02abcf233ae933d1c433
vidar soderquist <vidar.soderquist@appliedtechnology.se>
2019-07-15 15:28:09 +0200

    basic github auth woorking

---
+++ server/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    'GITHUB_KEY': '4339ce46cf25aa332b0c',
+    'GITHUB_SECRET': '5725a13a86e8263752b1fddcca51d464d555be57'
+  }
--- server/index.js
+++ server/index.js
@@ -1,7 +1,34 @@
-const express = require('express')
+const express = require("express")
 const app = express()
-const port = 3000
+var passport = require("passport")
+//var session = require("express-session")
+var GitHubStrategy = require("passport-github2").Strategy
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const GITHUB_CLIENT_ID = "4339ce46cf25aa332b0c" 
+const GITHUB_CLIENT_SECRET = "5725a13a86e8263752b1fddcca51d464d555be57" 
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+passport.use(
+  new GitHubStrategy(
+    {
+      clientID: GITHUB_CLIENT_ID,
+      clientSecret: GITHUB_CLIENT_SECRET,
+    },
+    function(accessToken, refreshToken, profile, done) {
+      console.log({ accessToken, refreshToken, profile })
+
+    }
+  )
+)
+
+app.get("/", (req, res) => {
+    res.send("<a href='/auth/github'>Sign in With GitHub</a>")
+})
+
+app.get(
+  "/auth/github",
+  passport.authenticate("github", { scope: ["repo:status"] }), /// Note the scope here
+  function(req, res) { }
+)
+
+const port = process.env.PORT || 5000
+app.listen(port, () => console.log(`listening on port ${port}`))
